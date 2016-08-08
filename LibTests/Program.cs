2d0813e399a0d2bc558879c7f70d3e6db3ff251c
grzegorz.tylak <grzegorz.tylak@expertsender.com>
2016-08-08 11:26:05 +0200

    Dodano projekt CommonRuns do uruchamiania kodu z Common

---
+++ LibTests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ExpertSender.Common.Helpers;
+
+namespace CommonRuns
+{
+    internal class Program
+    {
+        private static ModelsRegister _modelsRegister;
+
+        private static void Main()
+        {
+            _modelsRegister = new ModelsRegister();
+
+            var link = GetClickLink();
+        }
+
+        private static string GetClickLink()
+        {
+            var link = LinkHelper.GetTrackedLink(_modelsRegister.Domain, _modelsRegister.ServiceId, _modelsRegister.TrackedLinkId, _modelsRegister.MessageGuid, _modelsRegister.ListId, _modelsRegister.SubscriberId);
+            return link;
+        }
+
+        private class ModelsRegister
+        {
+            public string Domain { get; }
+            public int ServiceId { get; }
+            public int TrackedLinkId { get; }
+            public Guid MessageGuid { get; }
+            public int ListId { get; }
+            public int SubscriberId { get; }
+
+            public ModelsRegister()
+            {
+                Domain = "subscriber.dev.expertsender";
+                ServiceId = 7;
+                TrackedLinkId = 5;
+                MessageGuid = new Guid("12345678-1234-1234-1234-123456789012");
+                ListId = 6452; // External recipients
+                SubscriberId = 365567881; //grzegorz.tylak@expertsender.com
+            }
+        }
+    }
+}
