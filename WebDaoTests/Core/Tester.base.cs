5d78bf68069ec5ee1eeb7928088d4bd096ad05fb
grzegorz.tylak <grzegorz.tylak@expertsender.com>
2016-09-30 12:40:20 +0200

    Zapeniono jedną instancję ISession dla wszystkich użyć

--- WebDaoTests/Core/Tester.base.cs
+++ WebDaoTests/Core/Tester.base.cs
@@ -7,7 +7,7 @@ using StructureMap;
 
 namespace WebDaoTests.Core
 {
-    internal abstract class Tester
+    public abstract class Tester
     {
         protected IContainer Container { get; private set; }
         private ISessionFactory _sessionFactory;
@@ -32,6 +32,7 @@ namespace WebDaoTests.Core
         {
             var config = new NHibernate.Cfg.Configuration().Configure();
 
+            // zmieniamy ConnectionString na bazę wybranego unita
             var connstring = config.Properties[NHibernate.Cfg.Environment.ConnectionString];
             ReflectionHelper.SetValueOfPrivateStaticField(typeof(DynamicConnectionProvider), "_connectionString", connstring);
 
@@ -49,14 +50,16 @@ namespace WebDaoTests.Core
                     c.AddRegistry<DaoRegistry>();
 
                     c.Policies.FillAllPropertiesOfType<IContainer>();
-                    c.Policies.FillAllPropertiesOfType<ISession>().Use(context => GetContextSession(context));
+                    c.Policies.FillAllPropertiesOfType<ISession>()
+                        .Singleton()// zapewniamy jedną instancję ISession dla wszystkich wywołań
+                        .Use(context => GetContextSession(context));
                 });
                 StaticsProvider.Container = Container;
             }
         }
 
         /// <summary>
-        /// Nhibernate w ES2 nie uzywa transakcji (nie ma automatycznego rozpoczynania trnsakcji przy otwieraniu sesji). 
+        /// nHibernate w ES2 nie uzywa transakcji (nie ma automatycznego rozpoczynania transakcji przy otwieraniu sesji). 
         /// Jest to sprzeczne z dobrymi praktykami ale niesamowicie upraszcza przelaczanie sie pomiedzy unitami.
         /// Nie musimy troszczyc sie o komitowanie / rollbackowanie transakcji. Po prostu zmieniamy connection string w locie. 
         /// </summary>
