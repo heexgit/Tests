83efa1d98469db88d63844fb80b22f60bfe9aac6
grzegorz.tylak <grzegorz.tylak@expertsender.com>
2016-10-03 13:21:49 +0200

    kolejne zmiany

--- WebDaoTests/TestsLib/SubscriberManagerTests.cs
+++ WebDaoTests/TestsLib/SubscriberManagerTests.cs
@@ -3,9 +3,12 @@ using System.Linq;
 using ExpertSender.DataModel.Enums;
 using ExpertSender.Lib.SubscriberManager;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate;
 using WebDaoTests.Core;
 using EsAppContext = WebDaoTests.Mocks.EsAppContext;
 using Resources = ExpertSender.Lib.SubscriberManager.Resources;
+
+// ReSharper disable UnusedVariable, PossibleIntendedRethrow
 
 namespace WebDaoTests.TestsLib
 {
@@ -14,7 +17,32 @@ namespace WebDaoTests.TestsLib
     {
         private const int CurrentUnitId = 1;
 
-        private const int ActiveListId = 8;
+        private const string ExistSubscriber = "grzegorz.tylak@expertsender.com";
+        private const string NotExistSubscriber = "xyz19872842@expertsender.com";
+
+        /// <summary>
+        /// subskrybent jest zapisany do listy
+        /// </summary>
+        private const int ActiveSubscribedListId = 8;
+
+        /// <summary>
+        /// subskrybent NIE jest zapisany do listy
+        /// </summary>
+        private const int ActiveNotSubscribedListId = 14;
+
+        /// <summary>
+        /// subskrybent jest wypisany z listy z powodem Complaint
+        /// </summary>
+        private const int ActiveComplaintListId = 146;
+
+        /// <summary>
+        /// subskrybent jest wypisany z listy z powodem Api
+        /// </summary>
+        private const int ActiveUnsubscribedApiListId = 100;
+
+        /// <summary>
+        /// lista jest nieaktywna
+        /// </summary>
         private const int DeletedListId = 3;
 
         private const int RequiredPropertyString = 4;
@@ -36,11 +64,11 @@ namespace WebDaoTests.TestsLib
         {
             try
             {
-                var result = GetSubscriberManager_AddAndIgnore()
+                var result = SM_AddAndIgnore()
                     .AddSubscriber(
                         DeletedListId,
                         id: null, //SubscriberId
-                        email: "grzegorz.tylak@expertsender.com",
+                        email: ExistSubscriber,
                         emailMd5: null,
                         firstName: "Jan",
                         lastName: "Kowalski",
@@ -57,8 +85,6 @@ namespace WebDaoTests.TestsLib
                     && ex.FieldsErrors.SingleOrDefault(e => e.Key == "serviceId").Value.Contains(Resources.SubscriberManager.ListDoesNotBelongToService)
                 )
                     throw ex;
-                else
-                    Assert.Fail(ex.Message);
             }
         }
 
@@ -68,11 +94,11 @@ namespace WebDaoTests.TestsLib
         {
             try
             {
-                var result = GetSubscriberManager_AddAndIgnore()
+                var result = SM_AddAndIgnore()
                     .AddSubscriber(
-                        ActiveListId,
+                        ActiveSubscribedListId,
                         id: null, //SubscriberId
-                        email: "grzegorz.tylak@expertsender.com",
+                        email: ExistSubscriber,
                         emailMd5: null,
                         firstName: "Jan",
                         lastName: "Kowalski",
@@ -89,8 +115,6 @@ namespace WebDaoTests.TestsLib
                     && ex.PropertiesErrors.SingleOrDefault(e => e.Key == RequiredPropertyInt).Value.Contains(string.Format(Resources.SubscriberManager.FieldIsRequired, "cecha liczbowa xx3"))
                 )
                     throw ex;
-                else
-                    Assert.Fail(ex.Message);
             }
         }
 
@@ -100,11 +124,11 @@ namespace WebDaoTests.TestsLib
         {
             try
             {
-                var result = GetSubscriberManager_AddAndIgnore()
+                var result = SM_AddAndIgnore()
                     .AddSubscriber(
-                        ActiveListId,
+                        ActiveSubscribedListId,
                         id: null, //SubscriberId
-                        email: "grzegorz.tylak@expertsender.com",
+                        email: ExistSubscriber,
                         emailMd5: null,
                         firstName: "Jan",
                         lastName: "Kowalski",
@@ -125,19 +149,17 @@ namespace WebDaoTests.TestsLib
                     ex.PropertiesErrors.SingleOrDefault(e => e.Key == RequiredPropertyInt).Value.Contains(string.Format(Resources.SubscriberManager.InvalidValue, "cecha liczbowa xx3"))
                 )
                     throw ex;
-                else
-                    Assert.Fail(ex.Message);
             }
         }
 
         [TestMethod]
         public void AddSubscriber_WasIgnored_Test()
         {
-            var result = GetSubscriberManager_AddAndIgnore()
+            var result = SM_AddAndIgnore()
                 .AddSubscriber(
-                    ActiveListId,
+                    ActiveSubscribedListId,
                     id: null, //SubscriberId
-                    email: "grzegorz.tylak@expertsender.com",
+                    email: ExistSubscriber,
                     emailMd5: null,
                     firstName: "Jan",
                     lastName: "Kowalski",
@@ -158,11 +180,11 @@ namespace WebDaoTests.TestsLib
         [TestMethod]
         public void AddSubscriber_WasAlreadyOnList_Test()
         {
-            var result = GetSubscriberManager_AddAndUpdate()
+            var result = SM_AddAndUpdate()
                 .AddSubscriber(
-                    ActiveListId,
+                    ActiveSubscribedListId,
                     id: null, //SubscriberId
-                    email: "grzegorz.tylak@expertsender.com",
+                    email: ExistSubscriber,
                     emailMd5: null,
                     firstName: "Jan",
                     lastName: "Kowalski",
@@ -181,33 +203,228 @@ namespace WebDaoTests.TestsLib
         }
 
         [TestMethod]
-        public void AddSubscriber_1_Test()
+        public void AddSubscriber_WasAddedToList_Test()
         {
-            var result = GetSubscriberManager_AddAndUpdate()
-                .AddSubscriber(
-                    11,//ActiveListId,
-                    id: null, //SubscriberId
-                    email: "grzegorz.tylak@expertsender.com",
-                    emailMd5: null,
-                    firstName: "Jan",
-                    lastName: "Kowalski",
-                    trackingCode: null,
-                    vendor: null,
-                    ip: "123.9.88.99",
-                    propertiesDictionary: new Dictionary<int, object>
-                    {
-                        {RequiredPropertyString, "jakiś blok"},
-                        {RequiredPropertyInt, 7897987},
-                        {DeletedProperty, "jakiś blok"}
-                    }
-                );
+            AddSubscriberResult result;
+
+            using (var tran = Container.GetInstance<ISession>().BeginTransaction())
+            {
+                try
+                {
+                    result = SM_AddAndUpdate()
+                        .AddSubscriber(
+                            ActiveNotSubscribedListId,
+                            id: null, //SubscriberId
+                            email: ExistSubscriber,
+                            emailMd5: null,
+                            firstName: "Jan",
+                            lastName: "Kowalski",
+                            trackingCode: null,
+                            vendor: null,
+                            ip: "123.9.88.99",
+                            propertiesDictionary: new Dictionary<int, object>
+                            {
+                                {RequiredPropertyString, "jakiś blok"},
+                                {RequiredPropertyInt, 7897987},
+                                {DeletedProperty, "jakiś blok"}
+                            }
+                        );
+                }
+                finally
+                {
+                    // przywracamy zmienione obiekty do poprzedniego stanu
+                    tran.Rollback();
+                }
+            }
 
             Assert.AreEqual(true, result.WasAddedToList);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(SubscriberManagerException))]
+        public void AddSubscriber_EmailRejected_Test()
+        {
+            try
+            {
+                var result = SM_AddAndUpdate()
+                    .AddSubscriber(
+                        ActiveComplaintListId,
+                        id: null, //SubscriberId
+                        email: ExistSubscriber,
+                        emailMd5: null,
+                        firstName: "Jan",
+                        lastName: "Kowalski",
+                        trackingCode: null,
+                        vendor: null,
+                        ip: "123.9.88.99",
+                        propertiesDictionary: new Dictionary<int, object>
+                        {
+                            {RequiredPropertyString, "jakiś blok"},
+                            {RequiredPropertyInt, 7897987},
+                            {DeletedProperty, "jakiś blok"}
+                        }
+                    );
+            }
+            catch (SubscriberManagerException ex)
+            {
+                if (
+                    ex.FieldsErrors.SingleOrDefault(e => e.Key == "email").Value.Contains(Resources.SubscriberManager.EmailRejected)
+                )
+                    throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SubscriberManagerException))]
+        public void AddSubscriber_EmailRejected_Option_Test()
+        {
+            try
+            {
+                var result = SM_AddAndUpdate()
+                    .AddSubscriber(
+                        ActiveComplaintListId,
+                        id: null, //SubscriberId
+                        email: ExistSubscriber,
+                        emailMd5: null,
+                        firstName: "Jan",
+                        lastName: "Kowalski",
+                        trackingCode: null,
+                        vendor: null,
+                        ip: "123.9.88.99",
+                        propertiesDictionary: new Dictionary<int, object>
+                        {
+                            {RequiredPropertyString, "jakiś blok"},
+                            {RequiredPropertyInt, 7897987},
+                            {DeletedProperty, "jakiś blok"}
+                        }
+                    );
+            }
+            catch (SubscriberManagerException ex)
+            {
+                if (
+                    ex.FieldsErrors.SingleOrDefault(e => e.Key == "email").Value.Contains(Resources.SubscriberManager.EmailRejected)
+                )
+                    throw ex;
+            }
+        }
+
+        [TestMethod]
+        public void AddSubscriber_WasReAddedToList_Test()
+        {
+            AddSubscriberResult result;
+
+            using (var tran = Container.GetInstance<ISession>().BeginTransaction())
+            {
+                try
+                {
+                    result = SM_AddAndUpdate_AllowUnsubscribedByUser()
+                        .AddSubscriber(
+                            ActiveUnsubscribedApiListId,
+                            id: null, //SubscriberId
+                            email: ExistSubscriber,
+                            emailMd5: null,
+                            firstName: "Jan",
+                            lastName: "Kowalski",
+                            trackingCode: null,
+                            vendor: null,
+                            ip: "123.9.88.99",
+                            propertiesDictionary: new Dictionary<int, object>
+                            {
+                                {RequiredPropertyString, "jakiś blok"},
+                                {RequiredPropertyInt, 7897987},
+                                {DeletedProperty, "jakiś blok"}
+                            }
+                        );
+                }
+                finally
+                {
+                    // przywracamy zmienione obiekty do poprzedniego stanu
+                    tran.Rollback();
+                }
+            }
+
+            Assert.AreEqual(true, result.WasReAddedToList);
+        }
+
+        [TestMethod]
+        public void AddSubscriber_WasAdded_Test()
+        {
+            AddSubscriberResult result;
+
+            using (var tran = Container.GetInstance<ISession>().BeginTransaction())
+            {
+                try
+                {
+                    result = SM_AddAndUpdate()
+                        .AddSubscriber(
+                            ActiveNotSubscribedListId,
+                            id: null, //SubscriberId
+                            email: NotExistSubscriber,
+                            emailMd5: null,
+                            firstName: "Jan",
+                            lastName: "Kowalski",
+                            trackingCode: null,
+                            vendor: null,
+                            ip: "123.9.88.99",
+                            propertiesDictionary: new Dictionary<int, object>
+                            {
+                                {RequiredPropertyString, "jakiś blok"},
+                                {RequiredPropertyInt, 7897987},
+                                {DeletedProperty, "jakiś blok"}
+                            }
+                        );
+                }
+                finally
+                {
+                    // przywracamy zmienione obiekty do poprzedniego stanu
+                    tran.Rollback();
+                }
+            }
+
+            Assert.AreEqual(true, result.WasAdded);
+        }
+
+        [TestMethod]
+        public void AddSubscriber_ReplaceSubscriber_Test()
+        {
+            AddSubscriberResult result;
+
+            using (var tran = Container.GetInstance<ISession>().BeginTransaction())
+            {
+                try
+                {
+                    result = SM_AddAndReplace()
+                        .AddSubscriber(
+                            ActiveSubscribedListId,
+                            id: null, //SubscriberId
+                            email: ExistSubscriber,
+                            emailMd5: null,
+                            firstName: "Jan",
+                            lastName: "Kowalski",
+                            trackingCode: null,
+                            vendor: null,
+                            ip: "123.9.88.99",
+                            propertiesDictionary: new Dictionary<int, object>
+                            {
+                                {RequiredPropertyString, "jakiś blok"},
+                                {RequiredPropertyInt, 7897987},
+                                {DeletedProperty, "jakiś blok"}
+                            }
+                        );
+                }
+                finally
+                {
+                    // przywracamy zmienione obiekty do poprzedniego stanu
+                    tran.Rollback();
+                }
+            }
+
+            Assert.AreEqual(true, result.WasAlreadyOnList);
+        }
+
         #region GetSubscriberManager
 
-        private SubscriberManager GetSubscriberManager_AddAndIgnore()
+        private SubscriberManager SM_AddAndIgnore()
         {
             var subscriberManager = Container.GetInstance<SubscriberManager>();
             return subscriberManager
@@ -222,13 +439,43 @@ namespace WebDaoTests.TestsLib
                 );
         }
 
-        private SubscriberManager GetSubscriberManager_AddAndUpdate()
+        private SubscriberManager SM_AddAndUpdate()
         {
             var subscriberManager = Container.GetInstance<SubscriberManager>();
             return subscriberManager
                 .PreAddSubscriber(
                     CurrentUnitId,
                     SubscriberManagerMode.AddAndUpdate,
+                    SubscriptionSource.Api,
+                    forceConfirmation: false,
+                    addUnsubscribed: false,
+                    addUnsubscribedByUser: false,
+                    subscriptionLanguage: "en-US"
+                );
+        }
+
+        private SubscriberManager SM_AddAndUpdate_AllowUnsubscribedByUser()
+        {
+            var subscriberManager = Container.GetInstance<SubscriberManager>();
+            return subscriberManager
+                .PreAddSubscriber(
+                    CurrentUnitId,
+                    SubscriberManagerMode.AddAndUpdate,
+                    SubscriptionSource.Api,
+                    forceConfirmation: false,
+                    addUnsubscribed: false,
+                    addUnsubscribedByUser: true,
+                    subscriptionLanguage: "en-US"
+                );
+        }
+
+        private SubscriberManager SM_AddAndReplace()
+        {
+            var subscriberManager = Container.GetInstance<SubscriberManager>();
+            return subscriberManager
+                .PreAddSubscriber(
+                    CurrentUnitId,
+                    SubscriberManagerMode.AddAndReplace,
                     SubscriptionSource.Api,
                     forceConfirmation: false,
                     addUnsubscribed: false,
